b68f5dc0ccc4adb05b0c180c8da3d00e87fb5851
PYTHON <1@1.com>
2019-12-10 21:08:45 +0300

    my v_stack2

--- Tasks/a0_my_stack.py
+++ Tasks/a0_my_stack.py
@@ -4,61 +4,72 @@ My little Stack
 from typing import Any
 
 my_stack = []
-def push(elem: Any) -> None:
-	"""
-	Operation that add element to stack
 
-	:param elem: element to be pushed
-	:return: Nothing
-	"""
-	my_stack.append(elem)
-	print(elem)
-	return None
+
+def push(elem: Any) -> None:
+    """
+    Operation that add element to stack
+
+    :param elem: element to be pushed
+    :return: Nothing
+    """
+    global my_stack
+    my_stack.append(elem)
+    # print(elem)
+    return None
 
 
 def pop() -> Any:
-	"""
-	Pop element from the top of the stack
+    """
+    Pop element from the top of the stack
 
-	:return: popped element
-	"""
-	pop_elem = my_stack[-1]
-	del my_stack[-1]
-	return pop_elem
-
+    :return:
+    """
+    global my_stack
+    if len(my_stack) == 0:
+        # print(None)
+        return None
+    else:
+        pop_elem = my_stack[-1]
+        del my_stack[-1]
+        # print(pop_elem)
+        return pop_elem
 
 
 def peek(ind: int = 0) -> Any:
-	"""
-	Allow you to see at the element in the stack without popping it
+    """
+    Allow you to see at the element in the stack without popping it
 
-	:param ind: index of element (count from the top)
-	:return: peeked element
-	"""
-	# print(ind)
-	if ind > len(my_stack):
-		print('overflow')
-	else:
-		return my_stack[-ind-1]
+    :param ind: index of element (count from the top)
+    :return: peeked element
+    """
+    global my_stack
+    ind = ind % len(my_stack)
+    # print(my_stack[ind])
+    return my_stack[-ind-1]
 
 
 def clear() -> None:
-	"""
-	Clear my stack
+    """
+    Clear my stack
 
-	:return: None
-	"""
-	my_stack = []
-	return None
+    :return: None
+    """
+    global my_stack
+    my_stack = []
+    return None
 
 
 if __name__ == '__main__':
-	my_stack = [1, 2]
-	push(3)
-	print(my_stack)
-	print(pop())
-	print(my_stack)
-	print(peek(0))
-	print(peek(1))
-	print(peek(10))
+    print('test push')
+    for i in range(10):
+        push(i)
+    # print('test peek')
+    # for i in range(10):
+    #     peek(i)
+    # print('test pop')
+    # for i in range(15):
+    #     pop()
+    clear()
+    print(my_stack)
 
