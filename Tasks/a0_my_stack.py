8f11c73f993eac684d4d115da6a009c4d621e308
PYTHON <1@1.com>
2019-12-10 20:27:41 +0300

    my v_stack

--- Tasks/a0_my_stack.py
+++ Tasks/a0_my_stack.py
@@ -3,7 +3,7 @@ My little Stack
 """
 from typing import Any
 
-
+my_stack = []
 def push(elem: Any) -> None:
 	"""
 	Operation that add element to stack
@@ -11,6 +11,7 @@ def push(elem: Any) -> None:
 	:param elem: element to be pushed
 	:return: Nothing
 	"""
+	my_stack.append(elem)
 	print(elem)
 	return None
 
@@ -21,7 +22,10 @@ def pop() -> Any:
 
 	:return: popped element
 	"""
-	return None
+	pop_elem = my_stack[-1]
+	del my_stack[-1]
+	return pop_elem
+
 
 
 def peek(ind: int = 0) -> Any:
@@ -31,8 +35,11 @@ def peek(ind: int = 0) -> Any:
 	:param ind: index of element (count from the top)
 	:return: peeked element
 	"""
-	print(ind)
-	return None
+	# print(ind)
+	if ind > len(my_stack):
+		print('overflow')
+	else:
+		return my_stack[-ind-1]
 
 
 def clear() -> None:
@@ -41,4 +48,17 @@ def clear() -> None:
 
 	:return: None
 	"""
+	my_stack = []
 	return None
+
+
+if __name__ == '__main__':
+	my_stack = [1, 2]
+	push(3)
+	print(my_stack)
+	print(pop())
+	print(my_stack)
+	print(peek(0))
+	print(peek(1))
+	print(peek(10))
+
