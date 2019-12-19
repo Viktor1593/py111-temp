7ea13e066734bb2a55fada533b10de2ab9878f1c
PYTHON <1@1.com>
2019-12-19 20:53:43 +0300

    b2 1

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -2,17 +2,28 @@
 Taylor series
 """
 from typing import Union
+import math
 
-
-def ex(x: Union[int, float]) -> float:
+def ex(x: Union[int, float], delta = 0.0001) -> float:
 	"""
 	Calculate value of e^x with Taylor series
 
 	:param x: x value
 	:return: e^x value
 	"""
+
 	print(x)
-	return 0
+	if x == 0:
+		return 1
+	res_s = 0
+	n = 1
+	res = (x ** n) / math.factorial(n)
+	while res > delta:
+		res_s += res
+		res = (x ** n) / math.factorial(n)
+		n += 1
+
+	return res_s
 
 
 def sinx(x: Union[int, float]) -> float:
@@ -22,5 +33,11 @@ def sinx(x: Union[int, float]) -> float:
 	:param x: x value
 	:return: sin(x) value
 	"""
+
+
 	print(x)
 	return 0
+
+
+if __name__ == '__main__':
+	print(ex(1))
