46902f8effae84b6310171b1c29d16e8a14238e0
PYTHON <1@1.com>
2019-12-19 19:22:57 +0300

    b1 1

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -1,5 +1,7 @@
 from typing import Any, Sequence, Optional
 
+import numpy
+
 
 def binary_search(elem: Any, arr: Sequence) -> Optional[int]:
 	"""
@@ -9,5 +11,41 @@ def binary_search(elem: Any, arr: Sequence) -> Optional[int]:
 	:param arr: array where element is to be found
 	:return: Index of element if it's presented in the arr, None otherwise
 	"""
-	print(elem, arr)
+
+	ln = len(arr) // 2
+	ind = ln
+
+	for i in range(ln):
+		print(ln, ind, sep=': ')
+		if arr[ind] == elem:
+			return ind
+		if ln == 1:
+			break
+		ln = ln / 2
+		iln = int(ln)
+		print(ln - iln)
+		if ln - iln == 0.5:
+			ln = iln + 1
+		else:
+			ln = iln
+
+		if ln == 0:
+			ln = 1
+		if arr[ind] > elem:
+			ind -= ln
+		else:
+			ind += ln
+	# print(elem, arr)
 	return None
+
+if __name__ == '__main__':
+	n = 125
+	arr = []
+	for i in range(n):
+		arr.append(numpy.random.randint(i, n))
+	arr = numpy.sort(arr)
+	arr = numpy.unique(arr)
+	# array = numpy.arange(n)
+	# numpy.random.shuffle(array)
+	print(arr)
+	print('found: ', binary_search(75, arr))
