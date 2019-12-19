2afe038e74af54ee9ef016d90853ef0f2d4e10cf
PYTHON <1@1.com>
2019-12-19 21:20:43 +0300

    c1 1

--- Tasks/c1_fact.py
+++ Tasks/c1_fact.py
@@ -1,11 +1,17 @@
+
+
+
 def factorial_recursive(n: int) -> int:
 	"""
 	Calculate factorial of number n (> 0) in recursive way
 	:param n: int > 0
 	:return: factorial of n
 	"""
-	print(n)
-	return 0
+	if n < 0:
+		raise ValueError
+	elif n == 0:
+		return 1
+	return n * factorial_recursive(n-1)
 
 
 def factorial_iterative(n: int) -> int:
@@ -15,5 +21,11 @@ def factorial_iterative(n: int) -> int:
 	:param n: int > 0
 	:return: factorial of n
 	"""
-	print(n)
-	return 0
+	if n <= 0:
+		raise ValueError
+	# elif n == 0:
+	# 	return 1
+	p = 1
+	for i in range(1, n+1):
+		p *= i
+	return p
