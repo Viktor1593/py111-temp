9602b6c626e8ff0d8b5e2a2f5a6fbea2babdc927
Viktor1593 <vic-tor13@mail.ru>
2019-12-22 22:34:38 +0300

    trying b2

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -16,12 +16,12 @@ def ex(x: Union[int, float], delta = 0.0001) -> float:
     print(x)
     if x == 0:
         return 1
-    res_s = 0
+    res_s = 1
     n = 1
-    res = (x ** n) / math.factorial(n)
-    while res > delta:
+    res = (-1)**n * (x ** n) / math.factorial(n)
+    while abs(res) > delta:
         res_s += res
-        res = (x ** n) / math.factorial(n)
+        res = (-1)**n * (x ** n) / math.factorial(n)
         n += 1
 
     return res_s
@@ -34,21 +34,23 @@ def sinx(x: Union[int, float], delta = 0.0001) -> float:
     :param x: x value
     :return: sin(x) value
     """
-
     print(x)
-    if x == 0:
-        return 1
-    res_s = 0
-    n = 1
-    res = (x ** (2*n+1)) / math.factorial(2*n+1)
-    while res > delta:
-        res_s += res
-        res = (x ** (2*n+1)) / math.factorial(2*n+1)
-        n += 1
-    return res_s
+    y = 0
+    k = 0
+    # a=((-1)**k)*(x**(2*k + 1))/math.factorial(2*k + 1)
+    a=((-1)**k)*(x**(2*k + 1))/math.factorial(2*k + 1)
+    while abs(a) > delta:
+        y += a
+        a=((-1)**k)*(x**(2*k + 1))/math.factorial(2*k + 1)
+        k += 1
+        print(a)
+    print("finish")
+    return y
 
 
 if __name__ == '__main__':
-	const = 1.55433
-	print(ex(const))
-	print(sinx(const))
+    const = 1.55433
+    print(math.exp(const))
+    print(ex(const))
+    # print(sinx(const))
+    # print(math.sin(const))
