b95f2b51feb37e1906edf9c687f1b0f9d7ef221b
Viktor1593 <vic-tor13@mail.ru>
2020-01-13 20:34:05 +0300

    g2

--- Tasks/g2_quick_sort.py
+++ Tasks/g2_quick_sort.py
@@ -10,4 +10,19 @@ def sort(container: Collection[_Tt]) -> Collection[_Tt]:
 	:param container: container of elements to be sorted
 	:return: container sorted in ascending order
 	"""
-	return container
+	if len(container) <= 1:
+		return container
+	low = []
+	high = []
+	equal = []
+	base = container[0]
+	for i in range(len(container)):
+		if container[i] > base:
+			high.append(container[i])
+		elif container[i] < base:
+			low.append(container[i])
+		else:
+			equal.append(container[i])
+	return low + equal + high
+
+
