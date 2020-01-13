8fb2c87d93b65c892356fe744840c0d2ca7c8ed7
Viktor1593 <vic-tor13@mail.ru>
2020-01-13 18:34:50 +0300

    g0

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -10,6 +10,10 @@ def sort(container: Collection[_Tt]) -> Collection[_Tt]:
 	:param container: container of elements to be sorted
 	:return: container sorted in ascending order
 	"""
+	for i in range(len(container)):
+		for j in range(len(container) - i - 1):
+			if container[j] > container[j + 1]:
+				container[j], container[j + 1] = container[j + 1], container[j]
 
 	return container
 
@@ -18,7 +22,8 @@ if __name__ == '__main__':
 	import random
 	N = 10
 	sorted_list = list(range(N))
-	sorted_list = random.shuffle(sorted_list)
+	random.shuffle(sorted_list)
 	print(sorted_list)
+	print(sort(sorted_list))
 
 
