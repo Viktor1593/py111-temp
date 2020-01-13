b02aa337d06b3dea1a8b97c2aabd449b0969fc3b
Viktor1593 <vic-tor13@mail.ru>
2020-01-13 19:17:41 +0300

    g1

--- Tasks/g1_merge_sort.py
+++ Tasks/g1_merge_sort.py
@@ -10,4 +10,30 @@ def sort(container: Collection[_Tt]) -> Collection[_Tt]:
 	:param container: container of elements to be sorted
 	:return: container sorted in ascending order
 	"""
+	if len(container) <= 1:
+		return container
+	middle =
+
+
+	# 		middle = length(m) / 2
+	# 		for each x in m up to middle
+	# 		add
+	# 		x
+	# 		to
+	# 		left
+	# 	for each x in m after middle
+	# 	add
+	# 	x
+	# 	to
+	# 	right
+	#
+	#
+	# left = mergesort(left)
+	# right = mergesort(right)
+	# result = merge(left, right)
+	# return result
+
 	return container
+
+
+if __name__ == '__main__':
