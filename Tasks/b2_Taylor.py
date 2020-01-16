76bbe55acb0a5d5396b967d845fc272a1439f23d
Viktor1593 <vic-tor13@mail.ru>
2020-01-17 02:19:13 +0300

    b2 pass test

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -12,19 +12,18 @@ def ex(x: Union[int, float], delta = 0.0001) -> float:
     :param x: x value
     :return: e^x value
     """
-    print(x)
     if x == 0:
         return 1
-    res_s = 1
-    n = 1
-    res = (-1)**n * (x ** n) / math.factorial(n)
-    while abs(res) > delta:
-        #res_s += (-1)**n * (x ** n) / math.factorial(n)
-        res_s += res
-        res = (-1)**n * (x ** n) / math.factorial(n)
-        n += 1
+    s = 0
+    i = 0
+    while True:
+        a = x**i/math.factorial(i)
+        i += 1
+        s+=a
+        if abs(a) < delta:
+            break
 
-    return res_s
+    return s
 
 
 def sinx(x: Union[int, float], delta = 0.0001) -> float:
@@ -34,17 +33,14 @@ def sinx(x: Union[int, float], delta = 0.0001) -> float:
     :param x: x value
     :return: sin(x) value
     """
-    print(x)
-    y = 0
-    k = 0
-    a=((-1)**k)*(x**(1+2*k))/math.factorial(1+2*k)
+    a = x
+    s = a
+    i = 1
     while abs(a) > delta:
-        y+=((-1)**k)*(x**(1+2*k))/math.factorial(1+2*k)
-        #y = y + a
-        a=((-1)**k)*(x**(1+2*k))/math.factorial(1+2*k)
-        k += 1
-        #print(a)
-    return y
+        a *= -1 * x**2 / ((2 * i) * (2 * i + 1))
+        s += a
+        i+=1
+    return s
 
 if __name__ == '__main__':
     const = 1.55433
