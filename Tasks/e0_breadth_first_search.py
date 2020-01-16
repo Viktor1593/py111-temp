b58e6e463db493cfecca8a59497a5ae9044247d1
Viktor1593 <57449995+Viktor1593@users.noreply.github.com>
2020-01-16 20:25:36 +0300

    e0

--- Tasks/e0_breadth_first_search.py
+++ Tasks/e0_breadth_first_search.py
@@ -2,13 +2,59 @@ from typing import Hashable, List
 import networkx as nx
 
 
-def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
+def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
 	"""
-	Do an breadth-first search and returns list of nodes in the visited order
+	Do an depth-first search and returns list of nodes in the visited order
 
 	:param g: input graph
-	:param start_node: starting node for search
+	:param start_node: starting node of search
 	:return: list of nodes in the visited order
 	"""
 	print(g, start_node)
 	return list(g.nodes)
+
+
+def dfs_find(graph, src, dst, visited):
+	'''
+
+	:param graph:
+	:param src:
+	:param dst:
+	:param visited:
+	:return:
+	'''
+	if not src in visited:
+		visited.append(src)
+	# visited[src] = True
+	if src == dst:
+		return True
+	for node in graph.adj[src]:
+		if not node in visited:
+		# if not visited[node]:
+			if dfs_find(graph, node, dst, visited):
+				return True
+	return False
+
+
+if __name__ == '__main__':
+	graph = nx.Graph()
+	graph.add_nodes_from("ABCDEFG")
+	graph.add_edges_from(
+		[
+			('A', 'B'),
+			('A', 'C'),
+			('B', 'D'),
+			('B', 'E'),
+			('C', 'F'),
+			('E', 'G')
+		]
+	)
+	start = 'A'
+	dst = 'G'
+	graph.add_node('Z')
+	# print(graph.adj)
+	# visited = {node: False for node in graph.nodes()}
+	visited = []
+	print(dfs_find(graph, start, dst, visited))
+	print(visited)
+
