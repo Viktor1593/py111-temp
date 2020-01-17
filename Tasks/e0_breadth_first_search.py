457535c2821cbe762136a10b5883a01eb1ab3379
Viktor1593 <vic-tor13@mail.ru>
2020-01-17 04:09:25 +0300

    e0

--- Tasks/e0_breadth_first_search.py
+++ Tasks/e0_breadth_first_search.py
@@ -1,20 +1,20 @@
 from typing import Hashable, List
 import networkx as nx
+from collections import deque
 
-
-def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
+def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
 	"""
-	Do an depth-first search and returns list of nodes in the visited order
+	Do an breadth-first search and returns list of nodes in the visited order
 
 	:param g: input graph
-	:param start_node: starting node of search
+	:param start_node: starting node for search
 	:return: list of nodes in the visited order
 	"""
 	print(g, start_node)
 	return list(g.nodes)
 
 
-def dfs_find(graph, src, dst, visited):
+def bfs_find(graph, src, dst):
 	'''
 
 	:param graph:
@@ -23,6 +23,17 @@ def dfs_find(graph, src, dst, visited):
 	:param visited:
 	:return:
 	'''
+	visited = []
+	nodes_queue = deque()
+	nodes_queue.appendleft(src)
+	# while True:
+	# 	for node in graph.adj[src]:
+	# 		if not node in visited:
+	# 			visited.append(src)
+	# 			if node == src:
+	# 				return visited
+					
+
 	if not src in visited:
 		visited.append(src)
 	# visited[src] = True
@@ -53,8 +64,5 @@ if __name__ == '__main__':
 	dst = 'G'
 	graph.add_node('Z')
 	# print(graph.adj)
-	# visited = {node: False for node in graph.nodes()}
-	visited = []
 	print(dfs_find(graph, start, dst, visited))
-	print(visited)
 
