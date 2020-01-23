1ab7ba2029dc1106552b4662b3cf3ae21bcfce78
Viktor1593 <57449995+Viktor1593@users.noreply.github.com>
2020-01-23 19:16:22 +0300

    Update e0_breadth_first_search.py

--- Tasks/e0_breadth_first_search.py
+++ Tasks/e0_breadth_first_search.py
@@ -25,6 +25,24 @@ def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
 	return path
 
 
+def bfs_find(graph, src, dst):
+	visited = {node: False for node in graph.nodes()}
+	parents = {src: src}
+
+	deque_nodes = deque()
+	deque_nodes.appendleft(src)
+	while True:
+		try:
+			src = deque_nodes.pop()
+			visited[src] = True
+			for node in graph.adj[src]:
+				if not visited[node]:
+					deque_nodes.append(node)
+					parents[node] = parents[src] + node
+				if node == dst:
+					return parents[node]
+		except IndexError:
+			return None
 
 
 if __name__ == '__main__':
